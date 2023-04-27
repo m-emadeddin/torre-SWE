99d578c72e737234be6e022375e931151aeccc64
m-emadeddin <mohamed.emad12363@gmail.com>
2023-04-27 07:53:55 +0200

    Submitting Website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,16 @@ import { Component } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   constructor(private http: HttpClient) { }
   searchText :string= '';
   error:boolean=false;
-  title = 'torre-SWE';
+  title = 'torre-project';
   userData:any;
   selectedSkill: any;
 
@@ -23,10 +20,7 @@ export class AppComponent {
     this.selectedSkill = null;
     this.userData = null;
     this.error = false;
-    this.http.get(`https://bio.torre.co/api/bios/${this.searchText}`, {
-      observe: 'response',
-      responseType: 'json',
-      withCredentials: false,})
+    this.http.get(`https://bio.torre.co/api/bios/${this.searchText}`)
     .subscribe((res)=>{
       this.error = false;
       this.userData = res;
